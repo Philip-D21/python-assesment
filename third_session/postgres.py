bfb6ccfd1dcd1969bd2f7fe62e45442a090504ce
Philip-D21 <philiplekan88@gmail.com>
2024-03-13 20:37:49 +0100

    ...

--- third_session/postgres.py
+++ third_session/postgres.py
@@ -1,5 +1,7 @@
 import psycopg2
 
+
+
 host = "127.0.0.1"
 database = "postgres"
 user = "postgres"
@@ -38,10 +40,13 @@ def get_tasks():
     cur.execute("SELECT * FROM todos")
     return cur.fetchall()
 
+
 # delete a task by id
 def delete_task(task_id):
     cur.execute("DELETE FROM todos WHERE id = %s", (task_id,))
     conn.commit()
+
+
 
 # Close cursor and connection
 cur.close()
